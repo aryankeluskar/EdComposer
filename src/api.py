f75581c2a6bcaafb48568acc3e90081fb8a113e8
Aryan Keluskar <aryankeluskar6@gmail.com>
2024-06-20 18:02:53 +0530

    mvp for upload pdf and response

---
+++ src/api.py
@@ -0,0 +1,60 @@
+from http.client import HTTPException
+from fastapi import FastAPI, File, UploadFile, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+import aiofiles
+from embedchain import App as llm
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
+
+
+@app.get("/")
+async def root():
+    r"""
+    A function that serves the root endpoint of the API. It returns a FileResponse object that
+    represents the "index.html" file located in the "templates" directory. This function is
+    decorated with the `@app.get("/")` decorator, which means it will handle GET requests to the
+    root URL ("/"). The function is asynchronous, indicated by the `async` keyword before the
+    function definition.
+
+    Returns:
+        FileResponse: A FileResponse object representing the "index.html" file.
+    """
+    return FileResponse("templates/index.html")
+    
+@app.post("/upload")
+async def uploadInfo(
+    file: UploadFile = File(),
+    # prompt: str = "Simple and Straightforward Information",
+):
+    ans = None
+    try:
+        contents = await file.read()
+        # print(content.)
+        os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+        model = llm()
+        model.add(str(contents))
+        print("hello")
+        ans = str(model.query("how to get into grad school?"))
+        # delete the file after it's been processed
+        os.remove(file.filename)
+        print(ans)
+            
+            
+    except Exception as e:
+        print(e)
+    finally:
+        await file.close()
+
+    return {'message': f'Successfuly processed {file.filename}', 'answer': ans}
--- src/info.py
+++ src/info.py
@@ -1,2 +1,12 @@
-# make a function that has one of its arguments as pdf
+import tempfile
+from fastapi import File, UploadFile
+import os
 
+
+async def getInfo(file: UploadFile =  File(), prompt: str = "Simple and Straightforward Information"):
+    try:
+        with tempfile.NamedTemporaryFile() as tmp:
+            temp_path = os.path.join(tmp.name, file.filename)
+
+    except Exception as e:
+        print(e)
